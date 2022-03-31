afda01d583c1192053c1ddc192ba5dc3065a92fb
MarianoGomesOpitz <marianogomesopitz@gmail.com>
2022-03-31 17:46:23 -0300

    Primer commit

---
+++ tp2_1_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#define N 20
+
+int main()
+{
+    int i;
+    int vt[N];
+    srand(time(NULL));
+    for (i = 0; i < N; i++)
+    {
+        vt[i] = 1 + rand() % 100;
+        printf("%d ", vt[i]);
+    }
+    getchar();
+    return 0;
+}
